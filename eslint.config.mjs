973b7c8813726fdd5655488f8999c3573b2d454a
huseyinunnlu <huseyinunnlu@gmail.com>
2025-10-05 12:09:25 +0300

    feat: docs updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,7 +20,7 @@ const eslintConfig = [
             'next-env.d.ts',
         ],
         rules: {
-            'react-hooks/exhaustive-deps': 'warn',
+            'react-hooks/exhaustive-deps': 'off',
         },
     },
 ]
